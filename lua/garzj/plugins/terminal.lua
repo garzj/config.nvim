ae8a1b054ace555d22d04536969ca46f0426c689
garzj <johannes@garz.dev>
2024-08-12 01:25:46 +0200

    use tmux inside toggleterm for splits

--- lua/garzj/plugins/terminal.lua
+++ lua/garzj/plugins/terminal.lua
@@ -3,6 +3,7 @@ return {
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		opts = {
+			shell = [[tmux new-session \; set-option destroy-unattached \; set-option status-style bg=black]],
 			open_mapping = "<c-/>",
 			shade_terminals = false,
 			border = "curved",
