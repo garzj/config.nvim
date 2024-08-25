b2e062e955bd853f4590346883192b8682d7cd35
garzj <johannes@garz.dev>
2024-08-25 23:14:27 +0200

    remove tmux from toggleterm

--- lua/garzj/plugins/terminal.lua
+++ lua/garzj/plugins/terminal.lua
@@ -3,7 +3,6 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
-      shell = [[tmux new-session \; set-option destroy-unattached \; set-option status-style bg=black]],
       open_mapping = "<c-/>",
       shade_terminals = false,
       border = "curved",
