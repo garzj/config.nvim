6bdd8ff571c9854b5e367d36405461a68fb651b5
garzj <johannes@garz.dev>
2025-12-23 22:51:40 +0100

    add markdown options

--- lua/garzj/autocmds.lua
+++ lua/garzj/autocmds.lua
@@ -18,3 +18,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.expandtab = true
   end,
 })
+
+-- markdown word wrap
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
